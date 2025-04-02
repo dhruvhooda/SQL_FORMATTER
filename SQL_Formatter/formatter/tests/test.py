a5b7b0644a38e81ce8dc8bdebac9e13d4584011d
Dhruv Hooda <hooda@eduroam-169-233-153-44.ucsc.edu>
2025-04-01 18:33:34 -0700

    first

---
+++ SQL_Formatter/formatter/tests/test.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+
+def user_is_able_to_input():
---
+++ SQL_Formatter/user/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class UserSeralizer(serializers.ModelSerializer):
+    """Seralizer for the user object"""
+
+    class Meta:
+
