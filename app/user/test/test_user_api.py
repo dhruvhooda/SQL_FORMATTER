2856e63ba731f49ab201234821355b043ece65fb
Dhruv Hooda <hooda@Dhruvs-MacBook-Pro-9.local>
2025-04-06 18:10:31 -0700

    test update

--- app/user/test/test_user_api.py
+++ app/user/test/test_user_api.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from django.contrib.auth import get_user_model
 
 CREATE_USER_URL = reverse('user:create')
-CREATE_TOKEN_URL = reverse('user:token')
+#CREATE_TOKEN_URL = reverse('user:token')
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
@@ -14,18 +14,16 @@ class APITESTCASE(APITestCase):
         payload = {
             'username': 'testuser',
             'password': 'testpass123',
-            'name': 'testname',
         }
         res = self.client.post(CREATE_USER_URL, payload)
-        self.assertEqual(res, status.HTTP_201_CREATED)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
 
     def test_creating_existing_user(self):
         payload = {
             'username': 'testuser',
             'password': 'testpass123',
-            'name': 'testname',
         }
-        create_user(payload)
+        create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
-        self.assertEqual(res, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
--- app/user/urls.py
+++ app/user/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.urls import path
 from user import views
 
-
+app_name = "user"
 
 urlpatterns = [
     path('create/',views.CreateUserView.as_view(), name='create')
